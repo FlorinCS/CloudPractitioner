5c9529be083627214ebbf6926fde2884ddf8ad3c
FlorinCS <p.florinbest@gmail.com>
2025-06-20 15:15:32 +0200

    get rid of other props

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { useEffect, use, useState } from "react";
 import UserDashboard from "@/components/ui/dashboard";
-import { useUser } from "@/lib/auth";
 
 export default function Dashboard() {
-  const { userPromise } = useUser();
-  const user = use(userPromise);
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-base font-medium mb-6">Dashboard</h1>
