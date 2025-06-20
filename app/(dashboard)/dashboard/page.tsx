c3ef7701c14b3c9453e4fe799b91b49d1436fc90
FlorinCS <p.florinbest@gmail.com>
2025-06-20 15:13:57 +0200

    Get rid of unnecessary props

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-base font-medium mb-6">Dashboard</h1>
-      <UserDashboard user={user} />
+      <UserDashboard />
     </section>
   );
 }
